b843f84918738b629ba0256ec15ff9259d99b35a
Kun-9 <sus5330@icloud.com>
2022-09-01 20:54:58 +0900

    소켓통신 재연결, 오류 수정

--- main/gui_test.py
+++ main/gui_test.py
@@ -8,6 +8,7 @@ import json as j
 
 global gloStr
 gloStr = ''
+global check
 
 global serialArdu
 serialArdu = serial.Serial('/dev/ttyUSB_DEV1', 9600, timeout = 1)
@@ -22,7 +23,7 @@ s = sauce_manage_j.sauceList()
 
 global client_socket
 
-
+check = 0
 
 
 
@@ -35,97 +36,102 @@ class SokServer:
         self.PORT = PORT
         
     def startServer(self):
-        global client_socket
-        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-        # 포트 사용중이라 연결할 수 없다는 
-        # WinError 10048 에러 해결를 위해 필요합니다. 
-        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-
-        # bind 함수는 소켓을 특정 네트워크 인터페이스와 포트 번호에 연결하는데 사용됩니다.
-        # HOST는 hostname, ip address, 빈 문자열 ""이 될 수 있습니다.
-        # 빈 문자열이면 모든 네트워크 인터페이스로부터의 접속을 허용합니다. 
-        # PORT는 1-65535 사이의 숫자를 사용할 수 있습니다.  
-        server_socket.bind((self.HOST, self.PORT))
-
-        # 서버가 클라이언트의 접속을 허용하도록 합니다. 
-        server_socket.listen()
-
-        # accept 함수에서 대기하다가 클라이언트가 접속하면 새로운 소켓을 리턴합니다. 
-        acp = server_socket.accept()
-        client_socket = acp[0]
-        addr = acp[1]
-        # 접속한 클라이언트의 주소입니다.
-        print('Connected by', addr)
-
-
-        # 무한루프를 돌면서 
-        while True:
-
-            # 클라이언트가 보낸 메시지를 수신하기 위해 대기합니다. 
-            data = client_socket.recv(1024)
-
-            # 빈 문자열을 수신하면 루프를 중지합니다. 
-            if not data:
-                break
-
-
-            # 수신받은 문자열을 출력합니다.
-            print('Received from', addr, data.decode())
+        while 1:
             
-            global gloStr
-            gloStr = data.decode()
+            global client_socket
+            server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+            # 포트 사용중이라 연결할 수 없다는 
+            # WinError 10048 에러 해결를 위해 필요합니다. 
+            server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+
+            # bind 함수는 소켓을 특정 네트워크 인터페이스와 포트 번호에 연결하는데 사용됩니다.
+            # HOST는 hostname, ip address, 빈 문자열 ""이 될 수 있습니다.
+            # 빈 문자열이면 모든 네트워크 인터페이스로부터의 접속을 허용합니다. 
+            # PORT는 1-65535 사이의 숫자를 사용할 수 있습니다.  
+            server_socket.bind((self.HOST, self.PORT))
+
+            # 서버가 클라이언트의 접속을 허용하도록 합니다. 
+            server_socket.listen()
+
+            # accept 함수에서 대기하다가 클라이언트가 접속하면 새로운 소켓을 리턴합니다. 
+            acp = server_socket.accept()
+            client_socket = acp[0]
+            addr = acp[1]
+            # 접속한 클라이언트의 주소입니다.
+            print('Connected by', addr)
             
-            # 임시로 7로 설정
-            # 7이 들어왔을때 소스 수정
-            # 들어오는건 json 형식의 소스 정보와 아이디
-            # ex) {"name" : "간장", "isLiquid" : "1", "id" : "123456"}
-            if gloStr[:1] == '7':
-                editJson = j.loads(gloStr[1:])
+            global check
+            check = 0
+
+            # 무한루프를 돌면서 
+            while True:
+
+                # 클라이언트가 보낸 메시지를 수신하기 위해 대기합니다. 
+                data = client_socket.recv(1024)
+
+                # 빈 문자열을 수신하면 루프를 중지합니다. 
+                if not data:
+                    break
+
+
+                # 수신받은 문자열을 출력합니다.
+                print('Received from', addr, data.decode())
                 
-                print('editJson : ' + str(editJson))
+                global gloStr
+                gloStr = data.decode()
                 
-                id = editJson["id"]
-                s.editSauce(id, editJson)
-                s.getCurrentsauceList()
+                # 임시로 7로 설정
+                # 7이 들어왔을때 소스 수정
+                # 들어오는건 json 형식의 소스 정보와 아이디
+                # ex) {"name" : "간장", "isLiquid" : "1", "id" : "123456"}
                 
-            else:
-                # 아두이노에 문자열 전송
-                serialArdu.write(data)
-                print('gloStr = ',gloStr)
-            
-            # 원래 1~6 으로 시작함 
-            # 0으로 들어오면 소스 스캔 시작함
-            # 결과는 어떻게 내보내지?
-            # dataType
-            # 0 : 일반 메시지
-            # 1 : 소스 출력 문자열
-            # 2 : 소스 변경 문자열
-            # 3 : 
-            # 문자열의 가장 앞 문자가 2라면 소스 수정 메서드 실행
-            # 파싱 후 이름, 액체여부 값 파악
-            # if int()
-            
-            # sauce = s.getSauce(id)
-            # sauce.setName()
-            # sauce.setLiquid()
-                        
-            
-            
-            
-            
+                if gloStr[:1] == '7':
+                    editJson = j.loads(gloStr[1:])
+                    
+                    print('editJson : ' + str(editJson))
+                    
+                    id = editJson["id"]
+                    s.editSauce(id, editJson)
+                    
+                    # 스캔 시작
+                    # serialArdu.write("0".encode())
+                    
+                else:
+                    # 아두이노에 문자열 전송
+                    serialArdu.write(data)
+                    print('gloStr = ',gloStr)
+                    
+                    # 원래 1~6 으로 시작함 
+                    # 0으로 들어오면 소스 스캔 시작함
+                    # 결과는 어떻게 내보내지?
+                    # dataType
+                    # 0 : 일반 메시지
+                    # 1 : 소스 출력 문자열
+                    # 2 : 소스 변경 문자열
+                    # 3 : 
+                    # 문자열의 가장 앞 문자가 2라면 소스 수정 메서드 실행
+                    # 파싱 후 이름, 액체여부 값 파악
+                    # if int()
+                    
+                    # sauce = s.getSauce(id)
+                    # sauce.setName()
+                    # sauce.setLiquid()    
                 
-            # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코) 
-            # client_socket.sendall("client".encode())
-            # client_socket.send("string".decode())
-            # client_socket.sendall(data)
+                    
+                # 받은 문자열을 다시 클라이언트로 전송해줍니다.(에코) 
+                # client_socket.sendall("client".encode())
+                # client_socket.send("string".decode())
+                # client_socket.sendall(data)
 
 
-        # 소켓을 닫습니다.
-        client_socket.close()
-        server_socket.close()
+            # 소켓을 닫습니다.
+            # client_socket.close()
+            # server_socket.close()
+
+            
 
 
 class SerialInput:
@@ -156,10 +162,11 @@ class SerialInput:
                     sendStr = str(s.getCurrentsauceList()).replace('\'', '\"' )
                     client_socket.sendall(sendStr.encode())
                     # print(s.getCurrentsauceList())
-                    print(s.currentSauceList)
+                    print('서버에 저장되어있는 객체 배열 : ' + sendStr)
                 else :
                     
-                    print(content[2:], end="") 
+                    print(content, end="") 
+                    # [2:]
                 
 
 
@@ -186,42 +193,43 @@ InputThread = threading.Thread(target=inputString)
 
 ServerThread.start()
 InputThread.start()
-# # print(len(s.getsauceList()))
-# print(s.getsauceList()[0].getId())
-# print(s.getsauceList()[1].getId())
-
-# for i in range(6):   
-#     id = input()
-#     # a = sauce_manage_j.Sauce('null', 1, id)
-#     # print(a.getJson())
-#     # s.delSauce(id)
-#     # s.addSauce('null', 1, id)
-#     # print(s.getSauce(id))
-#     s.registCurrentSauce(i+1, id)   
 
     
-# s.saveCurrnetSourceList()
-# # client_socket.sendall(s.getCurrentsauceList().encode())
-# print(s.getCurrentsauceList())
+while True:
+    if check == 1:
+        check = 0
+        ServerThread.join()
+        ServerThread.start()
+        
 
-
-i = input()
-a = s.currentSauceList["body"] = [
-                "null",
-                "null",
-                "null",
-                "null",
-                "null",
-                "null" 
-]
-s.currentSauceList["body"][0] = {"name" : "hello", "h" : "d"}
-for i in range(6):
-    print(s.currentSauceList["body"][i])
-# sendStr = str(s.getCurrentsauceList()).replace('\'', '\"' )
-# client_socket.sendall(sendStr.encode())
-    
+def connect(self, ip, port):
+    retryCount = 0
+    retry = True
+    while(retry):
+        try:
+            print ('Connect Server')
+            self.socket.connect( (str(ip), int(port)) )
+            retry = False
+            return 1
+        except:
+            print ('Fail')
+            print ('retry after ')
+            retry = True
+            
+print( "connection lost... reconnecting" )
+connected = False  
+#recreate socket
+clientSocket = socket.socket()  
+while not connected:     
+    #attempt to reconnect, otherwise sleep for 2 seconds     
+    try:         
+        clientSocket.connect( ( host, port ) )         
+        connected = True         
+        print( "re-connection successful" )     
+    except socket.error:         
+        sleep( 2 )  
+        #continue normal operations 
 
         
-    
     
 
